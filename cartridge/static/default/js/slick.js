c99666ee3699641195cdb080717b0e7767e59982
nimit shukla <nimit.shukla@viseo.com>
2019-07-08 17:14:16 +0800

    Initial Commit

---
+++ cartridge/static/default/js/slick.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    $('.slick-carousel').slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        variableWidth: true,
+    });
+});
