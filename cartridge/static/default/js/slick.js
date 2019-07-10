0452486feb5530cb5f773c90713114a04ee53a80
nimit shukla <nimit.shukla@viseo.com>
2019-07-10 09:58:15 +0800

    removed slick carousel

--- cartridge/static/default/js/slick.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    $('.slick-carousel').slick({
-        dots: false,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        variableWidth: true
-    });
-});
